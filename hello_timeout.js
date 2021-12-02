e35164eba5bfb1bf75809988a976beff4fe65497
jabrilg <jabrilgadhane@hotmail.ca>
2021-12-02 03:28:23 +0000

    first commit

---
+++ hello_timeout.js
@@ -0,0 +1,12 @@
+
+ setTimeout(() => {
+    console.log ('4311o');
+ }, 5000);
+
+ setTimeout(() => {
+     console.log('th3r3');
+ }, 4000);
+
+setTimeout(() => {
+    console.log('wor1d');
+}, 3000);
