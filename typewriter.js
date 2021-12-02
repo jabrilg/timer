31f28b02d2223210465a5e6c48cb81c58dcd09f7
jabril gadhane <93634886+jabrilg@users.noreply.github.com>
2021-12-01 22:26:39 -0500

    Delete typewriter.js

--- typewriter.js
+++
@@ -1,7 +0,0 @@
-const sentence = "hello there from lighthouse labs\n";
-
-for (let x = 0; x < sentence. length; x++) {
-    setTimeout (() => {
-    process.stdout.write(sentence[x]);
-    }, x * 500);
-}
